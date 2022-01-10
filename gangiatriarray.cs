48502b59935b523781e8c7e7ec07e5ad6939eeaa
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-01-11 05:53:24 +0700

    Gán giá trị array

---
+++ gangiatriarray.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Text;
+
+namespace thaythephantu
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int[] mang = new int[1] { 100 };
+            mang[0] = 200;
+            Console.WriteLine(mang[0]);
+        }
+    }
+}
